07a9c00e0d459d80378ad5e039381bd862991297
Raspberry-Monster <lele2194512339@outlook.com>
2025-02-10 14:34:33 +0800

    WIP: Fix Upload Crash Exception

--- HyPlayer/Classes/CloudUpload.cs
+++ HyPlayer/Classes/CloudUpload.cs
@@ -11,7 +11,9 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using TagLib;
 using Windows.Storage;
+using File = TagLib.File;
 
 #endregion
 
@@ -24,33 +26,45 @@ namespace HyPlayer.Classes;
 /// </summary>
 internal class CloudUpload
 {
+#nullable enable
     public static async Task UploadMusic(StorageFile file)
     {
         Common.AddToTeachingTipLists("上传本地音乐至音乐云盘中", "正在上传: " + file.DisplayName);
+        var musicprop = await file.Properties.GetMusicPropertiesAsync();
         //首先获取基本信息
-        var abstraction = new UwpStorageFileAbstraction(file);
-        var tagFile = TagLib.File.Create(abstraction);
-        var tag = tagFile.Tag;
-        var album = tag.Album;
-        var duration = tagFile.Properties.Duration.TotalMilliseconds;
-        var bitrate = tagFile.Properties.AudioBitrate;
-        var name = tag.Title;
-        var artist = string.Join("; ", tag.Performers);
-        var coverBytes = tag.Pictures.FirstOrDefault().Data.Data;
-        tagFile.Dispose();
-        abstraction.Dispose();
+        using var abstraction = new UwpStorageFileAbstraction(file);
+        var album = string.Empty;
+        var duration = (long)musicprop.Duration.TotalMilliseconds;
+        var bitrate = musicprop.Bitrate;
+        var name = file.DisplayName;
+        var artist = string.Empty;
+        byte[]? coverBytes = null;
+        try
+        {
+            using var tagFile = File.Create(abstraction);
+            var tag = tagFile?.Tag;
+            album = tag?.Album;
+            name = tag?.Title;
+            artist = string.Join("; ", tag?.Performers ?? []);
+            coverBytes = tag?.Pictures.FirstOrDefault().Data?.Data;
+        }
+        catch
+        {
+            //Ignore
+        }
+        
         var bytes = await FileIO.ReadBufferAsync(file);
         //再获取上传所需要的信息
         var computedHash = new MD5CryptoServiceProvider().ComputeHash(bytes.ToArray());
         var sBuilder = new StringBuilder();
         foreach (var b in computedHash) sBuilder.Append(b.ToString("x2").ToLower());
         var md5 = sBuilder.ToString();
-        var checkResult = await Common.NeteaseAPI.RequestAsync(NeteaseApis.CloudUploadCheckApi,
+        var checkResult = await Common.NeteaseAPI!.RequestAsync(NeteaseApis.CloudUploadCheckApi,
             new CloudUploadCheckRequest()
             {
                 Ext = file.FileType,
                 Md5 = md5,
-                Bitrate = bitrate,
+                Bitrate = (int)bitrate,
             });
         if (checkResult.IsError)
         {
@@ -73,7 +87,7 @@ internal class CloudUpload
                 return;
             }
 
-            var objkey = tokenRes.Value.Data.ObjectKey;
+            var objkey = tokenRes.Value!.Data!.ObjectKey;
             // fetch load balancer
             var lb = "http://45.127.129.8";
             var loadBalancerReq = new NeteaseUploadLoadBalancerGetRequest()
@@ -84,7 +98,7 @@ internal class CloudUpload
                 loadBalancerReq);
             if (loadBalancerRes.IsSuccess)
             {
-                lb = loadBalancerRes.Value.Upload?.FirstOrDefault() ?? lb;
+                lb = loadBalancerRes.Value!.Upload?.FirstOrDefault() ?? lb;
             }
 
             var targetLink = $"{lb}/jd-musicrep-privatecloud-audio-public/{objkey}?offset=0&complete=true&version=1.0";
@@ -97,50 +111,52 @@ internal class CloudUpload
             request.Headers.Add("x-nos-token", tokenRes.Value.Data.Token);
             content.Headers.ContentType = new MediaTypeHeaderValue(file.ContentType);
             request.Content = content;
-            await Common.HttpClient.SendAsync(request);
+            await Common.HttpClient!.SendAsync(request);
             var title = string.IsNullOrEmpty(name)
                 ? Path.GetFileNameWithoutExtension(file.Path)
                 : name;
-
-            // upload cover
-            var imgcomputedHash = new MD5CryptoServiceProvider().ComputeHash(coverBytes);
-            var imgsBuilder = new StringBuilder();
-            foreach (var b in imgcomputedHash) imgsBuilder.Append(b.ToString("x2").ToLower());
-            var imgmd5 = imgsBuilder.ToString();
-
             string coverId = string.Empty;
-            var coverAllocRes = await Common.NeteaseAPI.RequestAsync(NeteaseApis.CloudUploadCoverTokenAllocApi,
-                new CloudUploadCoverTokenAllocRequest
-                {
-                    Ext = "png",
-                    Filename = $"{file.DisplayName}_cover",
-                });
-            if (coverAllocRes.IsError)
+            // upload cover
+            if (coverBytes != null)
             {
-                Common.AddToTeachingTipLists($"上传失败(封面): {file.DisplayName}", coverAllocRes.Error!.Message);
-            }
-            coverId = coverAllocRes.Value?.Result?.DocId;
-            var imglb = "http://45.127.129.8";
-            var imgloadBalancerReq = new NeteaseUploadLoadBalancerGetRequest()
-            {
-                Bucket = "yyimg"
-            };
-            var imgloadBalancerRes = await Common.NeteaseAPI.RequestAsync(NeteaseApis.NeteaseUploadLoadBalancerGetApi,
-                imgloadBalancerReq);
-            if (imgloadBalancerRes.IsSuccess)
-            {
-                imglb = imgloadBalancerRes.Value.Upload?.FirstOrDefault() ?? lb;
-            }
-            targetLink = $"{imglb}/yyimg/{coverAllocRes.Value?.Result?.ObjectKey}?offset=0&complete=true&version=1.0";
+                var imgcomputedHash = new MD5CryptoServiceProvider().ComputeHash(coverBytes);
+                var imgsBuilder = new StringBuilder();
+                foreach (var b in imgcomputedHash) imgsBuilder.Append(b.ToString("x2").ToLower());
+                var imgmd5 = imgsBuilder.ToString();
 
-            using var imgReq = new HttpRequestMessage(HttpMethod.Post,
-                new Uri(targetLink));
-            using var imgContent = new ByteArrayContent(coverBytes);
-            content.Headers.Add("Content-MD5", md5);
-            request.Headers.Add("x-nos-token", coverAllocRes.Value?.Result?.Token);
-            content.Headers.ContentType = new MediaTypeHeaderValue("image/png");
-            request.Content = imgContent;
-            await Common.HttpClient.SendAsync(request);
+                var coverAllocRes = await Common.NeteaseAPI.RequestAsync(NeteaseApis.CloudUploadCoverTokenAllocApi,
+                    new CloudUploadCoverTokenAllocRequest
+                    {
+                        Ext = "png",
+                        Filename = $"{file.DisplayName}_cover",
+                    });
+                if (coverAllocRes.IsError)
+                {
+                    Common.AddToTeachingTipLists($"上传失败(封面): {file.DisplayName}", coverAllocRes.Error!.Message);
+                }
+                coverId = coverAllocRes.Value?.Result?.DocId!;
+                var imglb = "http://45.127.129.8";
+                var imgloadBalancerReq = new NeteaseUploadLoadBalancerGetRequest()
+                {
+                    Bucket = "yyimg"
+                };
+                var imgloadBalancerRes = await Common.NeteaseAPI.RequestAsync(NeteaseApis.NeteaseUploadLoadBalancerGetApi,
+                    imgloadBalancerReq);
+                if (imgloadBalancerRes.IsSuccess)
+                {
+                    imglb = imgloadBalancerRes.Value!.Upload?.FirstOrDefault() ?? lb;
+                }
+                targetLink = $"{imglb}/yyimg/{coverAllocRes.Value?.Result?.ObjectKey}?offset=0&complete=true&version=1.0";
+
+                using var imgReq = new HttpRequestMessage(HttpMethod.Post,
+                    new Uri(targetLink));
+                using var imgContent = new ByteArrayContent(coverBytes);
+                content.Headers.Add("Content-MD5", md5);
+                request.Headers.Add("x-nos-token", coverAllocRes.Value?.Result?.Token);
+                content.Headers.ContentType = new MediaTypeHeaderValue("image/png");
+                request.Content = imgContent;
+                await Common.HttpClient.SendAsync(request);
+            }
 
 
             var infoReq = new CloudUploadInfoRequest
@@ -148,11 +164,11 @@ internal class CloudUpload
                 Md5 = md5,
                 SongId = checkResult.Value!.SongId!,
                 FileName = file.Name,
-                Song = title,
-                Album = album,
+                Song = title ?? file.DisplayName,
+                Album = album ?? "",
                 Artist = artist,
-                Bitrate = bitrate,
-                CoverId = coverId,
+                Bitrate = (int)bitrate,
+                CoverId = coverId!,
                 ResourceId = tokenRes.Value.Data!.ResourceId!,
                 ObjectKey = tokenRes.Value.Data!.ObjectKey!,
             };
@@ -164,7 +180,7 @@ internal class CloudUpload
             }
             var cloudPubReq = new CloudPubRequest()
             {
-                SongId = infoRes.Value.SongId!
+                SongId = infoRes.Value!.SongId!
             };
             var cloudPubRes = await Common.NeteaseAPI.RequestAsync(NeteaseApis.CloudPubApi, cloudPubReq);
             if (cloudPubRes.IsError)
@@ -177,4 +193,5 @@ internal class CloudUpload
             }
         }
     }
+#nullable restore
 }
